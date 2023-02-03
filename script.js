bfac01d906912e5e96148565c302d4e5231ea354
OLUWATOMISIN <hardex.tommy@gmail.com>
2023-02-03 11:58:47 +0100

    03-02

--- script.js
+++ script.js
@@ -10,7 +10,19 @@ const mainDiv = document.querySelectorAll(".mainDiv");
 
 const accordion = document.querySelector(".accordion");
 
-const number = document.querySelector ('.number')
+// const panel = document.querySelector ('.panel')
+
+// const notficationNumber = document.querySelector ('.number')
+
+const dot = document.querySelectorAll('i');
+
+const hide = document.querySelector('.hide');
+
+// const pp =document.querySelector('.pp');
+
+const markAllAsRead =document.querySelector ('.top-nav2');
+
+const oP = document.querySelectorAll('.oP');
 
 
 
@@ -18,18 +30,83 @@ const number = document.querySelector ('.number')
 
 
 
+
+markAllAsRead.addEventListener('click', function(){
+
+mainDiv.forEach((ctn) =>{
+if (ctn.classList.contains("lightBlueBg")) {
+ ctn.classList.remove("lightBlueBg");
+}}
+)
+
+
+dot.forEach((dot) => {
+  const closest = dot.closest(".mainDiv");
+  if (closest)
+ {
+      dot.style.visibility = "hidden";
+    };
+});
+
+ document.querySelector(".number").textContent = 0;
+
+
+
+
+
+})
+
+
+
+
+
+// Notification number
+
+let number=[];
+
+mainDiv.forEach((curr)=>{
+  if (curr.classList.contains("lightBlueBg")){
+number.push(curr);
+
+  }
+
+})
+ let total = number.length;
+ document.querySelector(".number").textContent = total;
+
+
+
+
+
+
+
+  //  remove of bacground
 
 mainDiv.forEach((ctn) =>
 ctn.addEventListener('click', function(){
-  console.log('load');
-  ctn.classList.remove('lightBlueBg')
+
+if (ctn.classList.contains("lightBlueBg")) {
+ ctn.classList.remove("lightBlueBg");
+
+  number = Number(document.querySelector(".number").textContent);
+  number--;
+
+  document.querySelector(".number").textContent = number;
+
+ if (number < 0) {
+   document.querySelector(".number").textContent = 0;
+ }
+
+}
 
 
-  data = Number(document.querySelector(".number").textContent);
-  data--;
 
-  document.querySelector(".number").textContent = data;
-
+oP.forEach((oP) => {
+  const close = oP.closest(".mainDiv");
+  if (close) {
+    oP.classList.remove("hide");
+  }
+});
 
 
 })
@@ -38,6 +115,17 @@ ctn.addEventListener('click', function(){
 
 
 
+
+
+// remove individual icons by clicking on it
+
+dot.forEach((dot) => {
+  const closest = dot.closest(".mainDiv");
+  if (closest)
+    closest.addEventListener("click", function () {
+      dot.style.visibility = "hidden";
+    });
+});
 
 
 
